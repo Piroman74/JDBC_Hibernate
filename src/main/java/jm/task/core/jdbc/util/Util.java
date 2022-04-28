b87de7bb4ed0539a3e23d750a3adca0e35d08fe2
Dimas Pol <dmitrijpol74@gmail.com>
2022-04-29 00:12:30 +0500

    done

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private String url = "jdbc:mysql://localhost:3306/firstschema";
+    private String username = "root";
+    private String password = "1234";
+    private Connection connection;
+
+    public Util() {
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
